de46bd8a7c231a4810666bd498013f0a6a28c04b
Aakash Sigdel <aakash.sigdel@gmail.com>
2017-06-24 03:28:52 +0545

    add button component

---
+++ src/colors.js
@@ -0,0 +1,6 @@
+const colors = {
+  PRIMARY: '#E25057',
+  FONTCOLOR: '#9a9a9a'
+};
+
+export default colors;
---
+++ src/components/Button.js
@@ -0,0 +1,88 @@
+import React, { PureComponent } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+import colors from '../colors';
+
+const Touchable = Platform.OS === 'android'
+  ? TouchableNativeFeedback
+  : TouchableOpacity;
+
+class Button extends PureComponent {
+  render () {
+    const { backgroundColor, color, disabled, onPress, title } = this.props;
+
+    const buttonStyle = [styles.buttonStyle];
+    if (backgroundColor) {
+      buttonStyle.push({ backgroundColor })
+    }
+
+    const textStyle = [styles.text];
+    if (color) {
+      textStyle.push({ color });
+    }
+
+    if (disabled) {
+      buttonStyle.push(styles.disabled);
+      textStyle.push(styles.disabledText);
+    }
+
+    return(
+      <Touchable onPress={onPress} disabled={disabled}>
+        <View style={buttonStyle}>
+          <Text style={textStyle}>
+            { title.toUpperCase() }
+          </Text>
+        </View>
+      </Touchable>
+    )
+  }
+}
+
+Button.defaultProps = {
+  title: 'Button',
+};
+
+Button.propTypes = {
+  title: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  color: PropTypes.string,
+  disabled: PropTypes.bool
+};
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    elevation: 4,
+    backgroundColor: colors.PRIMARY,
+    shadowColor: '#999999',
+    shadowOpacity: 1,
+    shadowOffset: {width: 0, height: 1}
+  },
+  text: {
+    color: '#FFF',
+    textAlign: 'center',
+    padding: 8,
+    fontWeight: '500'
+  },
+  disabled: {
+    elevation: 0,
+    backgroundColor: '#dfdfdf'
+  },
+  disabledText: Platform.select({
+    ios: {
+      color: '#cdcdcd'
+    },
+    android: {
+      color: '#a1a1a1'
+    }
+  })
+});
+
+export default Button;
