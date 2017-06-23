1904307654d67899e46003ef192028073d69bc40
Aakash Sigdel <aakash.sigdel@gmail.com>
2017-06-24 04:24:11 +0545

    add input component

--- src/colors.js
+++ src/colors.js
@@ -1,5 +1,6 @@
 const colors = {
   PRIMARY: '#E25057',
+  SECONDARY: '#D8D8D8',
   FONTCOLOR: '#9a9a9a'
 };
 
---
+++ src/components/Input.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  View
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+import colors from '../colors';
+import lock from '../../assets/images/lock.png';
+import user from '../../assets/images/user.png';
+
+class Input extends Component {
+  _renderImage (type) {
+    if (type === 'password') {
+      return <Image source={lock} style={styles.icon} />
+    } else if (type === 'email') {
+      return <Image source={user} style={styles.icon} />
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    const { placeholder, style, type, ...props } = this.props;
+    return (
+      <View style={styles.container}>
+        {this._renderImage(type)}
+        <TextInput
+          placeholderTextColor="gray"
+          placeholder="Email address"
+          style={[styles.textInput, style]} {...props}
+          tintColor={colors.PRIMARY}
+        />
+      </View>
+    );
+  }
+}
+
+Input.propTypes = {
+  placeholder: PropTypes.string,
+  style: PropTypes.object,
+  type: PropTypes.string
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: colors.SECONDARY,
+    borderRadius: 2,
+    paddingLeft: 15,
+    paddingRight: 10,
+    paddingBottom: 4,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: 15
+  },
+  textInput: {
+    width: '100%',
+    height: 45,
+    paddingTop: 6
+  }
+});
+
+export default Input;
